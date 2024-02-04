2ada903cde59834e9477ba7065955f6d66edf824
Your Name <your_email@whatever.com>
2024-02-04 19:49:18 +0600

    initial commit

--- lab4/1f.py
+++
@@ -1,4 +0,0 @@
-#task 1
-grams = float(input()) #input() для ввода от руки, нет параметров
-ounces = 28.3495231 * grams #наша формула, grams - переменная
-print(ounces) #принт принимает аргумент ounces
--- lab4/6f.py
+++
@@ -1,9 +0,0 @@
-#task 6
-def r(str):
-    words = str.split()  # Разбиваем строку на слова
-    res = ' '.join(reversed(words))  # Обратный порядок слов и объединяем их в предложение
-    return res
-
-s = input()
-res = r(s)
-print(res)
