eaba9f75f3f63a090ba4e2aa8527c05a4e702094
nieyrinn <nieyrinn@nieyrinn-2.local>
2024-03-25 19:27:14 +0500

    remake

--- lab7/music.py
+++ lab7/music.py
@@ -3,16 +3,18 @@ import os
 
 pygame.init()
 
-screen_width, screen_height = 640, 480
+screen_width = 640
+screen_height = 480
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("music player")
 
 music_folder = "music_player"
-music_files = [f for f in os.listdir(music_folder) if f.endswith(".mp3")]
+music_files = sorted([f for f in os.listdir(music_folder) if f.endswith(".mp3")])
 num_songs = len(music_files)
 current_song = 0
-
 pygame.mixer.music.load(os.path.join(music_folder, music_files[current_song]))
+
+font = pygame.font.Font(None, 36)
 
 clock = pygame.time.Clock()
 
@@ -22,9 +24,15 @@ def play_song():
 def stop_song():
     pygame.mixer.music.stop()
 
-def switch_song(direction):
+def next_song():
     global current_song
-    current_song = (current_song + direction) % num_songs
+    current_song = (current_song + 1) % num_songs
+    pygame.mixer.music.load(os.path.join(music_folder, music_files[current_song]))
+    pygame.mixer.music.play()
+
+def prev_song():
+    global current_song
+    current_song = (current_song - 1) % num_songs
     pygame.mixer.music.load(os.path.join(music_folder, music_files[current_song]))
     pygame.mixer.music.play()
 
@@ -40,13 +48,12 @@ while True:
                 else:
                     play_song()
             elif event.key == pygame.K_n:
-                switch_song(1)
+                next_song()
             elif event.key == pygame.K_p:
-                switch_song(-1)
+                prev_song()
     
     screen.fill((255, 255, 255))
     song_text = music_files[current_song]
-    font = pygame.font.Font(None, 36)
     song_label = font.render(song_text, True, (0, 0, 0))
     screen.blit(song_label, (screen_width//2 - song_label.get_width()//2, screen_height//2 - song_label.get_height()//2))
     pygame.display.update()
